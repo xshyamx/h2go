c37f745fc88b5fce0fbfba181dfb77cb1de6639f
shyam <xshyamx@gmail.com>
2024-12-01 15:32:42 +0530

    use baseDir and ignore default basedir

--- contrib/runStandalone.sh
+++ contrib/runStandalone.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-java -classpath h2.jar org.h2.tools.Server -tcp   -tcpAllowOthers -ifNotExists -trace -baseDir tmpData
+mkdir -p h2-db
+java -classpath h2.jar org.h2.tools.Server \
+		 -tcp -tcpAllowOthers -ifNotExists \
+		 -trace -baseDir ./h2-db
